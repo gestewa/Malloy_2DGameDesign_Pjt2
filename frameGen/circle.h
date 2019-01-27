c7651f77719c98729f1581d79d076f03e4308240
Gabrielle Stewart <gestewa@clemson.edu>
2019-01-27 17:10:17 -0500

    Added guards to all header files

--- frameGen/circle.h
+++ frameGen/circle.h
@@ -1,14 +1,16 @@
+#pragma once
+
 #include <SDL.h>
 #include "point.h"
 
 class Circle
 {
-  public:
-    Circle(Point s, int r, SDL_Color c);
-    Draw(SDL_Renderer *);
+public:
+  Circle(Point s, int r, SDL_Color c);
+  Draw(SDL_Renderer *);
 
-  private:
-    Point center;
-    int radius;
-    SDL_Color color;
+private:
+  Point center;
+  int radius;
+  SDL_Color color;
 };
--- frameGen/frameGenerator.h
+++ frameGen/frameGenerator.h
@@ -1,16 +1,20 @@
+#pragma once
+
 #include <string>
 #include <SDL.h>
 
-class FrameGenerator {
+class FrameGenerator
+{
 public:
-  FrameGenerator(SDL_Renderer*, SDL_Window*, int, int, const std::string&);
+  FrameGenerator(SDL_Renderer *, SDL_Window *, int, int, const std::string &);
   void makeFrame();
+
 private:
-  SDL_Renderer* const renderer;
-  SDL_Window* const window;
+  SDL_Renderer *const renderer;
+  SDL_Window *const window;
   const int WIDTH;
   const int HEIGHT;
   const std::string USERNAME;
-  FrameGenerator(const FrameGenerator&);
-  FrameGenerator& operator=(const FrameGenerator&);
+  FrameGenerator(const FrameGenerator &);
+  FrameGenerator &operator=(const FrameGenerator &);
 };
